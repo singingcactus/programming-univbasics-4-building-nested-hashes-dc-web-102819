fd9f9af3434f1ba147c459a267c88cb1ce0d67da
Matt Bechtel <bechma02@gmail.com>
2019-10-20 20:15:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,6 +33,9 @@ def monopoly_with_fourth_tier
 	base_hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
 	base_hash[:railroads][:names] = {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
 	base_hash[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+	base_hash[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
+	base_hash[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+	base_hash[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
 
 
 
