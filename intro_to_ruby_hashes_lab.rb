2de73a3b70df2a76baeca0d22a493633f3912306
Matt Bechtel <bechma02@gmail.com>
2019-10-20 19:53:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def base_hash
-	my_hash = {railroads: "something goes here"}
-	my_hash
+	base_hash = {railroads: "something goes here"}
+	base_hash
 	# Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 end
