6b67dd840cfa9b20b0cc8ea5a4c5b66ecebcba34
Matt Bechtel <bechma02@gmail.com>
2019-10-20 20:17:42 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,24 +1,45 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
+	base_hash = {railroads: {}}
+	base_hash
+	# Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
+	base_hash = {railroads: {}}
+	base_hash[:railroads][:pieces] = 4
+	base_hash
+	# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
+	base_hash = {railroads: {}}
+	base_hash[:railroads][:pieces] = 4
+	base_hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
+	base_hash[:railroads][:names] = {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
+	base_hash
+	# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 end
 
 def monopoly_with_fourth_tier
+	base_hash = {railroads: {}}
+	base_hash[:railroads][:pieces] = 4
+	base_hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
+	base_hash[:railroads][:names] = {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
+	base_hash[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+	base_hash[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
+	base_hash[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
+	base_hash[:railroads][:names][:shortline_railroad]["mortgage_value"] = "$800"
+
+
+
+	base_hash
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
